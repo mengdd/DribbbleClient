70d04bf0877c3f317b44bf84c1b5f80cd7f0ec36
Dandan MENG <mengdandanno1@163.com>
2018-08-09 17:11:50 +0800

    add drawer layout and toolbar

--- app/src/main/java/com/ddmeng/dribbbleclient/MainActivity.kt
+++ app/src/main/java/com/ddmeng/dribbbleclient/MainActivity.kt
@@ -1,12 +1,18 @@
 package com.ddmeng.dribbbleclient
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.Gravity
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.toolbar.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+        toolbar.setNavigationIcon(R.drawable.ic_menu)
+        toolbar.setNavigationOnClickListener { drawer_layout.openDrawer(Gravity.START) }
     }
 }
