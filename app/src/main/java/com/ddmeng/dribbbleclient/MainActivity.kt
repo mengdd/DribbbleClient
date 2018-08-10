7d7ac0ae4f0b013b551c715841a289406ec597f0
Dandan MENG <mengdandanno1@163.com>
2018-08-10 16:15:50 +0800

    use data binding

--- app/src/main/java/com/ddmeng/dribbbleclient/MainActivity.kt
+++ app/src/main/java/com/ddmeng/dribbbleclient/MainActivity.kt
@@ -1,18 +1,28 @@
 package com.ddmeng.dribbbleclient
 
+import android.databinding.DataBindingUtil
 import android.os.Bundle
+import android.support.v4.widget.DrawerLayout
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.Toolbar
 import android.view.Gravity
-import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.toolbar.*
+import com.ddmeng.dribbbleclient.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var toolbar: Toolbar
+    private lateinit var drawerLayout: DrawerLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        val binding: ActivityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        drawerLayout = binding.drawerLayout
+        toolbar = binding.toolbarLayout!!.toolbar
+
         setSupportActionBar(toolbar)
         toolbar.setNavigationIcon(R.drawable.ic_menu)
-        toolbar.setNavigationOnClickListener { drawer_layout.openDrawer(Gravity.START) }
+        toolbar.setNavigationOnClickListener { drawerLayout.openDrawer(Gravity.START) }
     }
 }
