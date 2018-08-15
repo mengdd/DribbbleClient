5311bc0598a0c7f341be26156da1cb6f0acf1249
Dandan MENG <mengdandanno1@163.com>
2018-08-15 13:46:23 +0800

    get the OAuth token

---
+++ app/src/main/java/com/ddmeng/dribbbleclient/data/model/OAuthToken.kt
@@ -0,0 +1,18 @@
+package com.ddmeng.dribbbleclient.data.model
+
+import com.google.gson.annotations.Expose
+import com.google.gson.annotations.SerializedName
+
+class OAuthToken {
+
+    @SerializedName("access_token")
+    @Expose
+    var accessToken: String? = null
+    @SerializedName("token_type")
+    @Expose
+    var tokenType: String? = null
+    @SerializedName("scope")
+    @Expose
+    var scope: String? = null
+
+}
