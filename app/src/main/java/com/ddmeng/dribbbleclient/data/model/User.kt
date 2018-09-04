af958f0d6ecc8e7b9e8307af53b6149254cd1031
Dandan MENG <mengdandanno1@163.com>
2018-09-04 16:36:54 +0800

    add tests for UserRepository

--- app/src/main/java/com/ddmeng/dribbbleclient/data/model/User.kt
+++ app/src/main/java/com/ddmeng/dribbbleclient/data/model/User.kt
@@ -9,51 +9,50 @@ import com.google.gson.annotations.SerializedName
 
 @Entity(tableName = "user_table")
 @TypeConverters(DataTypeConverter::class)
-class User {
+data class User(
+        @SerializedName("avatar_url")
+        @Expose
+        var avatarUrl: String? = null,
+        @SerializedName("bio")
+        @Expose
+        var bio: String? = null,
+        @SerializedName("can_upload_shot")
+        @Expose
+        var canUploadShot: Boolean? = null,
+        @SerializedName("created_at")
+        @Expose
+        var createdAt: String? = null,
+        @SerializedName("followers_count")
+        @Expose
+        var followersCount: Int? = null,
+        @SerializedName("html_url")
+        @Expose
+        var htmlUrl: String? = null,
+        @PrimaryKey
+        @SerializedName("id")
+        @Expose
+        @NonNull
+        var id: Int = 0,
+        @SerializedName("links")
+        @Expose
+        var links: Map<String, String>? = null,
+        @SerializedName("location")
+        @Expose
+        var location: String? = null,
+        @SerializedName("login")
+        @Expose
+        var login: String? = null,
+        @SerializedName("name")
+        @Expose
+        var name: String? = null,
+        @SerializedName("pro")
+        @Expose
+        var pro: Boolean? = null,
+        @SerializedName("type")
+        @Expose
+        var type: String? = null,
+        @SerializedName("teams")
+        @Expose
+        var teams: List<String>? = null
 
-    @SerializedName("avatar_url")
-    @Expose
-    var avatarUrl: String? = null
-    @SerializedName("bio")
-    @Expose
-    var bio: String? = null
-    @SerializedName("can_upload_shot")
-    @Expose
-    var canUploadShot: Boolean? = null
-    @SerializedName("created_at")
-    @Expose
-    var createdAt: String? = null
-    @SerializedName("followers_count")
-    @Expose
-    var followersCount: Int? = null
-    @SerializedName("html_url")
-    @Expose
-    var htmlUrl: String? = null
-    @PrimaryKey
-    @SerializedName("id")
-    @Expose
-    @NonNull
-    var id: Int = 0
-    @SerializedName("links")
-    @Expose
-    var links: Map<String, String>? = null
-    @SerializedName("location")
-    @Expose
-    var location: String? = null
-    @SerializedName("login")
-    @Expose
-    var login: String? = null
-    @SerializedName("name")
-    @Expose
-    var name: String? = null
-    @SerializedName("pro")
-    @Expose
-    var pro: Boolean? = null
-    @SerializedName("type")
-    @Expose
-    var type: String? = null
-    @SerializedName("teams")
-    @Expose
-    var teams: List<String>? = null
-
-}
+)
---
+++ app/src/test/java/com/ddmeng/dribbbleclient/data/repository/UserRepositoryTest.kt
@@ -0,0 +1,84 @@
+package com.ddmeng.dribbbleclient.data.repository
+
+import android.arch.core.executor.testing.InstantTaskExecutorRule
+import android.arch.lifecycle.MutableLiveData
+import android.arch.lifecycle.Observer
+import android.webkit.CookieManager
+import com.ddmeng.dribbbleclient.AppExecutors
+import com.ddmeng.dribbbleclient.data.local.UserDao
+import com.ddmeng.dribbbleclient.data.model.User
+import com.ddmeng.dribbbleclient.data.remote.UserService
+import com.ddmeng.dribbbleclient.data.valueobject.Resource
+import com.ddmeng.dribbbleclient.utils.ApiUtil
+import com.ddmeng.dribbbleclient.utils.InstantAppExecutors
+import com.ddmeng.dribbbleclient.utils.TestUtil
+import com.ddmeng.dribbbleclient.utils.mock
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.mockito.Mock
+import org.mockito.Mockito
+import org.mockito.Mockito.verify
+import org.mockito.MockitoAnnotations
+
+class UserRepositoryTest {
+
+    private lateinit var appExecutors: AppExecutors
+    @Mock
+    private lateinit var userDao: UserDao
+    @Mock
+    private lateinit var userService: UserService
+    @Mock
+    private lateinit var cookieManager: CookieManager
+
+    private lateinit var userRepository: UserRepository
+
+    @Before
+    fun setUp() {
+        MockitoAnnotations.initMocks(this)
+        appExecutors = InstantAppExecutors()
+
+        userRepository = UserRepository(appExecutors, userDao, userService, cookieManager)
+    }
+
+    @Rule
+    @JvmField
+    val instantExecutorRule = InstantTaskExecutorRule()
+
+    @Test
+    fun loadUser() {
+        userRepository.getUser(false)
+        verify(userDao).query()
+    }
+
+    @Test
+    fun goToNetwork() {
+        val dbData = MutableLiveData<User>()
+        Mockito.`when`(userDao.query()).thenReturn(dbData)
+        val user = TestUtil.createUser("foo")
+        val call = ApiUtil.successCall(user)
+        Mockito.`when`(userService.getUser()).thenReturn(call)
+        val observer = mock<Observer<Resource<User>>>()
+
+        userRepository.getUser(true).observeForever(observer)
+
+        verify(userService, Mockito.never()).getUser()
+        val updatedDbData = MutableLiveData<User>()
+        Mockito.`when`(userDao.query()).thenReturn(updatedDbData)
+        dbData.value = null
+        verify(userService).getUser()
+    }
+
+    @Test
+    fun doNotGoToNetwork() {
+        val dbData = MutableLiveData<User>()
+        val user = TestUtil.createUser("foo")
+        dbData.value = user
+        Mockito.`when`(userDao.query()).thenReturn(dbData)
+        val observer = mock<Observer<Resource<User>>>()
+
+        userRepository.getUser(false).observeForever(observer)
+        verify(userService, Mockito.never()).getUser()
+        verify(observer).onChanged(Resource.success(user))
+    }
+}
