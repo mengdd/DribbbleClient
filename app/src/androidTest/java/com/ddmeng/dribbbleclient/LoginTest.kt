27d1f60f97cfdf00ec2e7ddf3046ca552c6432e7
Chen Chen <talent_prince@163.com>
2018-09-25 09:04:12 +0800

    Add test rule for databinding, livedata and excutors

--- app/src/androidTest/java/com/ddmeng/dribbbleclient/LoginTest.kt
+++ app/src/androidTest/java/com/ddmeng/dribbbleclient/LoginTest.kt
@@ -3,8 +3,11 @@ package com.ddmeng.dribbbleclient
 import android.support.test.InstrumentationRegistry
 import android.support.test.espresso.Espresso.onView
 import android.support.test.espresso.action.ViewActions.click
+import android.support.test.espresso.assertion.ViewAssertions.matches
 import android.support.test.espresso.contrib.DrawerActions
+import android.support.test.espresso.matcher.ViewMatchers.isDisplayed
 import android.support.test.espresso.matcher.ViewMatchers.withId
+import android.support.test.espresso.matcher.ViewMatchers.withText
 import android.support.test.espresso.web.assertion.WebViewAssertions.webMatches
 import android.support.test.espresso.web.model.Atoms.getCurrentUrl
 import android.support.test.espresso.web.sugar.Web.onWebView
@@ -15,7 +18,11 @@ import android.support.test.espresso.web.webdriver.Locator
 import android.support.test.filters.LargeTest
 import android.support.test.rule.ActivityTestRule
 import android.support.test.runner.AndroidJUnit4
+import com.ddmeng.dribbbleclient.util.CountingAppExecutorsRule
+import com.ddmeng.dribbbleclient.util.DataBindingIdlingResourceRule
+import com.ddmeng.dribbbleclient.util.TaskExecutorWithIdlingResourceRule
 import org.hamcrest.CoreMatchers.containsString
+import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -31,6 +38,21 @@ class LoginTest {
             InstrumentationRegistry.getTargetContext().deleteDatabase("dribbble_client_database")
             InstrumentationRegistry.getTargetContext().deleteSharedPreferences("dribbble_client_shared_preferences")
         }
+    }
+
+    @Rule
+    @JvmField
+    val countingAppExecutors = CountingAppExecutorsRule()
+    @Rule
+    @JvmField
+    val executorRule = TaskExecutorWithIdlingResourceRule()
+    @Rule
+    @JvmField
+    val dataBindingIdlingResourceRule = DataBindingIdlingResourceRule(activityTestRule)
+
+    @Before
+    fun setUp() {
+        activityTestRule.activity.appExecutors = countingAppExecutors.appExecutors
     }
 
     @Test
@@ -60,9 +82,7 @@ class LoginTest {
                 .withElement(findElement(Locator.CSS_SELECTOR, "input[type=\"submit\"]"))
                 .perform(webClick())
 
-        //TODO Random failed on Caused by: kotlin.TypeCastException: null cannot be cast to non-null type com.ddmeng.dribbbleclient.MainActivity
-        //at com.ddmeng.dribbbleclient.features.auth.OAuthFragment.exit(OAuthFragment.kt:76)
-//        onView(withId(R.id.drawer_layout)).perform(DrawerActions.open())
-//        onView(withText("Dandan Meng")).check(matches(isDisplayed()))
+        onView(withId(R.id.drawer_layout)).perform(DrawerActions.open())
+        onView(withText("Dandan Meng")).check(matches(isDisplayed()))
     }
 }
