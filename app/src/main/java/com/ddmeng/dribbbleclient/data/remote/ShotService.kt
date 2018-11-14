ee22aeb2650f5c4a24b8a97abcbb8f03c98228a5
Dandan MENG <mengdandanno1@163.com>
2018-11-14 09:23:19 +0800

    fix the 404 error due to slash in the beginning of path, this will only use the host and omit v2 in path

--- app/src/main/java/com/ddmeng/dribbbleclient/data/remote/ShotService.kt
+++ app/src/main/java/com/ddmeng/dribbbleclient/data/remote/ShotService.kt
@@ -5,6 +5,6 @@ import com.ddmeng.dribbbleclient.data.model.Shot
 import retrofit2.http.GET
 
 interface ShotService {
-    @GET("/user/shots")
+    @GET("user/shots")
     fun getUserShots(): LiveData<ApiResponse<List<Shot>>>
 }
